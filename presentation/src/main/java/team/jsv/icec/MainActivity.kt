a9e92d0d00da30a28381671a04913d32955fbeaf
jiiiiiyoon <63251413+jiiiiiyoon@users.noreply.github.com>
2023-01-31 20:39:49 +0900

    메인 - 사진 촬영 및 갤러리 사진 선택 화면 layout  (#3)
    * main layout Init
    
    * refactor : 불필요한 import 제거

--- presentation/src/main/java/team/jsv/icec/MainActivity.kt
+++ presentation/src/main/java/team/jsv/icec/MainActivity.kt
@@ -1,11 +1,31 @@
 package team.jsv.icec
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import team.jsv.presentation.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.ratioButton.setOnClickListener {
+            // 비율 옵션 버튼
+        }
+
+        viewBinding.galleryButton.setOnClickListener {
+            // 갤러리 버튼
+        }
+
+        viewBinding.captureButton.setOnClickListener {
+            // 사진 찍기 버튼
+        }
+
+        viewBinding.reverseButton.setOnClickListener {
+            // 좌우 반전 버튼
+        }
     }
 }
