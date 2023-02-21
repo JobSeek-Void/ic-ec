2bdf808dbbd1350fac99d4d6bee21299f0fb81ae
DESKTOP-QIC63AA\user <wldbs5897@naver.com>
2023-02-21 15:23:12 +0900

    refactor: Acitivity 이름 변경 및 databinding 적용

--- presentation/src/main/java/team/jsv/icec/ui/takepicture/TakePictureActivity.kt
+++ presentation/src/main/java/team/jsv/icec/ui/takepicture/TakePictureActivity.kt
@@ -1,31 +1,269 @@
+<<<<<<<< HEAD:presentation/src/main/java/team/jsv/icec/ui/takepicture/TakePictureActivity.kt
+@file:SuppressLint("ResourceType")
+
 package team.jsv.icec.ui.takepicture
+========
+package team.jsv.icec.ui.main
+>>>>>>>> 661e1b4 (refactor: Acitivity 이름 변경 및 databinding 적용):presentation/src/main/java/team/jsv/icec/ui/main/MainActivity.kt
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+<<<<<<<< HEAD:presentation/src/main/java/team/jsv/icec/ui/takepicture/TakePictureActivity.kt
+import android.provider.MediaStore
+import android.util.Log
+import android.util.Size
+import androidx.annotation.RequiresApi
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import team.jsv.icec.base.BaseActivity
+import team.jsv.presentation.R
 import team.jsv.presentation.databinding.ActivityTakePictureBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class TakePictureActivity : AppCompatActivity() {
-    private lateinit var viewBinding: ActivityTakePictureBinding
+enum class RatioId(val id: Int) {
+    OneOnOne(1),
+    FourOnThree(2),
+    NineOnSixteen(3)
+}
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+class TakePictureActivity :
+    BaseActivity<ActivityTakePictureBinding>(R.layout.activity_take_picture) {
+    private lateinit var cameraExecutor: ExecutorService
+    private var imageCapture: ImageCapture? = null
+    private var cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+========
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import team.jsv.icec.base.BaseActivity
+import team.jsv.icec.ui.main.mosaic.MosaicViewModel
+import team.jsv.icec.util.PermissionUtil
+import team.jsv.icec.util.requestPermissions
+import team.jsv.presentation.R
+import team.jsv.presentation.databinding.ActivityMainBinding
+import java.io.File
+
+@AndroidEntryPoint
+class MainActivity :
+    BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+>>>>>>>> 661e1b4 (refactor: Acitivity 이름 변경 및 databinding 적용):presentation/src/main/java/team/jsv/icec/ui/main/MainActivity.kt
+
+    private val viewModel: MosaicViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityTakePictureBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+<<<<<<<< HEAD:presentation/src/main/java/team/jsv/icec/ui/takepicture/TakePictureActivity.kt
+        startCameraWithPermission()
+        getLastImageFromGallery()
+    }
 
-        viewBinding.ratioButton.setOnClickListener {
-            // 비율 옵션 버튼
+    override fun onResume() {
+        super.onResume()
+        initClickListener()
+        resizeCameraView(RatioId.FourOnThree.id)
+    }
+
+    private fun initClickListener() {
+        binding.imageviewOneOnOne.setOnClickListener {
+            resizeCameraView(RatioId.OneOnOne.id)
         }
 
-        viewBinding.galleryButton.setOnClickListener {
-            // 갤러리 버튼
+        binding.imageviewThreeOnFour.setOnClickListener {
+            resizeCameraView(RatioId.FourOnThree.id)
         }
 
-        viewBinding.captureButton.setOnClickListener {
-            // 사진 찍기 버튼
+        binding.imageviewNineOnSixteen.setOnClickListener {
+            resizeCameraView(RatioId.NineOnSixteen.id)
         }
 
-        viewBinding.reverseButton.setOnClickListener {
-            // 좌우 반전 버튼
+        binding.buttonCapture.setOnClickListener {
+            takePhoto()
         }
+
+        binding.buttonReverse.setOnClickListener {
+            if (cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA) {
+                cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                startCamera()
+            } else {
+                cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+                startCamera()
+            }
+        }
+
+        binding.imageviewGallery.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun resizeCameraView(id: Int) {
+        val display = binding.root.resources?.displayMetrics
+        val deviceWidth = display?.widthPixels
+
+        val layoutParams = binding.cameraPreview.layoutParams
+
+        when (id) {
+            RatioId.OneOnOne.id -> {
+                layoutParams.height = deviceWidth!!
+            }
+
+            RatioId.FourOnThree.id -> {
+                layoutParams.height = deviceWidth!! / 3 * 4
+            }
+
+            RatioId.NineOnSixteen.id -> {
+                layoutParams.height = deviceWidth!! / 9 * 16
+            }
+        }
+
+        binding.cameraPreview.layoutParams = layoutParams
+    }
+
+    private fun getLastImageFromGallery() {
+        val uriExternal: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+            MediaStore.Images.ImageColumns._ID,
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.ImageColumns.DATE_ADDED,
+            MediaStore.Images.ImageColumns.MIME_TYPE
+        )
+        val cursor: Cursor = this.contentResolver.query(
+            uriExternal, projection, null,
+            null, MediaStore.Images.ImageColumns.DATE_ADDED + " DESC"
+        )!!
+
+        if (cursor.moveToFirst()) {
+            val columnIndexID = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            val imageId: Long = cursor.getLong(columnIndexID)
+            val imageURI = Uri.withAppendedPath(uriExternal, "" + imageId)
+
+            Glide.with(this)
+                .load(imageURI)
+                .into(binding.imageviewGallery)
+        }
+        cursor.close()
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().apply {
+                setTargetResolution(Size(1080, 1080))
+            }.build()
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+            } catch (e: Exception) {
+                Log.e("cameraError", "Use case binding failed ", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.KOREA)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e("captureError", "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    getLastImageFromGallery()
+                }
+            }
+        )
+    }
+
+    private fun startCameraWithPermission() {
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun openGallery() {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, PICK_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageUri = data?.data ?: return
+        //      고른 이미지 처리
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val PICK_IMAGE = 100
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA
+            ).apply {
+                add(getImageStoragePermission())
+            }.toTypedArray()
+
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+}
+========
+
+        // TODO(ham2174) : 갤러리 혹은 카메라로 찍은 사진 데이터를 가져와야함. TakePictureActivity -> MainActivity 이미지 데이터 전달 필요
+        requestPermissions(PermissionUtil.getPermissions())
+>>>>>>>> 661e1b4 (refactor: Acitivity 이름 변경 및 databinding 적용):presentation/src/main/java/team/jsv/icec/ui/main/MainActivity.kt
+
+        viewModel.setImage(File("/storage/emulated/0/Pictures/test_4.jpg"))
     }
 }
