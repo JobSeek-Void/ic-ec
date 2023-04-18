045f20e096b1bf39a7d224c6ebd7ae804282f5ee
GunHyung Ham <gdreagon@naver.com>
2023-04-19 02:00:07 +0900

    feat : 검출된 얼굴 리사이클러뷰 데이터 연동

--- presentation/src/main/java/team/jsv/icec/ui/main/mosaic/selectFace/SelectFaceFragment.kt
+++ presentation/src/main/java/team/jsv/icec/ui/main/mosaic/selectFace/SelectFaceFragment.kt
@@ -5,6 +5,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import team.jsv.icec.base.BaseFragment
 import team.jsv.icec.base.EventObserver
 import team.jsv.icec.ui.main.mosaic.MosaicViewModel
+import team.jsv.icec.util.HorizontalSpaceItemDecoration
 import team.jsv.presentation.R
 import team.jsv.presentation.databinding.FragmentSelectFaceBinding
 
@@ -13,11 +14,24 @@ class SelectFaceFragment :
     BaseFragment<FragmentSelectFaceBinding>(R.layout.fragment_select_face) {
 
     private val viewModel: MosaicViewModel by activityViewModels()
+    private val detectedFaceAdapter by lazy { DetectedFaceAdapter() }
 
     override fun initView() {
+        binding.rvDetectedFace.apply {
+            adapter = detectedFaceAdapter
+        }
+
         viewModel.backPress.observe(this, EventObserver {
             popBackStack()
         })
+
+        viewModel.detectFaces.observe(this) {
+            detectedFaceAdapter.submitList(it.faceList)
+
+            val detectedFaceCount = it.faceList.size
+            binding.tvDetectedFaceCount.text =
+                getString(R.string.detected_face_count, detectedFaceCount)
+        }
     }
 
 }
