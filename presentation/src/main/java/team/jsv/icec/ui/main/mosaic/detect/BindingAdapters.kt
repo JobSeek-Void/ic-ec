32b5f4841ce42c23a0f4300fb482a5778a67e142
GunHyung Ham <gdreagon@naver.com>
2023-04-27 03:02:24 +0900

    feat : 검출된 얼굴수 `detectedFaceCount` 데이터 바인딩으로 적용

--- presentation/src/main/java/team/jsv/icec/ui/main/mosaic/detect/BindingAdapters.kt
+++ presentation/src/main/java/team/jsv/icec/ui/main/mosaic/detect/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package team.jsv.icec.ui.main.mosaic.detect
 
+import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
 import com.google.android.material.button.MaterialButton
@@ -7,9 +8,16 @@ import team.jsv.presentation.R
 
 @BindingAdapter("allSelectBackground")
 fun MaterialButton.changeBackground(isAllSelect: Boolean) {
-    if (isAllSelect) {
+    text = if (isAllSelect) {
         setBackgroundColor(ResourcesCompat.getColor(resources, R.color.SubColor, null))
+        context.getString(R.string.deselect_all_text)
     } else {
         setBackgroundColor(ResourcesCompat.getColor(resources, R.color.gray1, null))
+        context.getString(R.string.select_all_text)
     }
+}
+
+@BindingAdapter("detectedFaceCount")
+fun TextView.applyFaceCount(count: Int) {
+    text = context.getString(R.string.detected_face_count, count)
 }
