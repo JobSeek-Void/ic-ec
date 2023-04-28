8efa9f71dcf1365736cb29d858a48ed6558222bd
GunHyung Ham <gdreagon@naver.com>
2023-04-28 20:24:36 +0900

    refactor : `getDetectedFace` API response data `code` 재설정

--- data/src/main/java/team/jsv/data/dto/response/FaceResponse.kt
+++ data/src/main/java/team/jsv/data/dto/response/FaceResponse.kt
@@ -3,6 +3,7 @@ package team.jsv.data.dto.response
 import com.google.gson.annotations.SerializedName
 
 data class FaceResponse(
+    @SerializedName("code") val code: Int,
     @SerializedName("originalImage") val originalImage: String,
     @SerializedName("faceList") val faceList: List<FaceInfo>
 )
