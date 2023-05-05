98d835de86589c442e4261a52f9c127774b63e88
DESKTOP-QIC63AA\user <wldbs5897@naver.com>
2023-05-05 23:19:21 +0900

    refactor: ratio 초기화하는 방법 재설정

--- presentation/src/main/java/team/jsv/icec/ui/camera/CameraViewModel.kt
+++ presentation/src/main/java/team/jsv/icec/ui/camera/CameraViewModel.kt
@@ -6,12 +6,9 @@ import team.jsv.icec.base.BaseViewModel
 
 
 class CameraViewModel : BaseViewModel() {
-    private val _ratioState = MutableLiveData<Int>()
+    private val _ratioState = MutableLiveData<Int>().apply { value = SettingRatio.RATIO_1_1.id }
     val ratioState: LiveData<Int> get() = _ratioState
 
-    fun initSetRatio() {
-        _ratioState.value = SettingRatio.RATIO_1_1.id
-    }
 
     fun setRatioState(value: Int) {
         _ratioState.value = value
