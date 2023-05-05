eb6a568a636e25bc363b2278ccbff10f635f1ba0
DESKTOP-QIC63AA\user <wldbs5897@naver.com>
2023-05-05 23:19:55 +0900

    feat: CameraFragment, CameraResultFragment 생성

--- presentation/src/main/java/team/jsv/icec/ui/camera/CameraViewModel.kt
+++ presentation/src/main/java/team/jsv/icec/ui/camera/CameraViewModel.kt
@@ -1,5 +1,6 @@
 package team.jsv.icec.ui.camera
 
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import team.jsv.icec.base.BaseViewModel
@@ -9,8 +10,21 @@ class CameraViewModel : BaseViewModel() {
     private val _ratioState = MutableLiveData<Int>().apply { value = SettingRatio.RATIO_1_1.id }
     val ratioState: LiveData<Int> get() = _ratioState
 
+    private val _bitmapImage = MutableLiveData<Bitmap>()
+    val bitmapImage: LiveData<Bitmap> get() = _bitmapImage
+
+    private val _isFrontCamera = MutableLiveData<Boolean>().apply { value = true }
+    val isFrontCamera: LiveData<Boolean> get() = _isFrontCamera
 
     fun setRatioState(value: Int) {
         _ratioState.value = value
+    }
+
+    fun setBitmapImage(image: Bitmap) {
+        _bitmapImage.value = image
+    }
+
+    fun setIsFrontCamera(value : Boolean) {
+        _isFrontCamera.value = value
     }
 }
