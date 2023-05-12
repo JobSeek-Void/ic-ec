8eb117a323b7646188b2eb42665605f24531b333
DESKTOP-QIC63AA\user <wldbs5897@naver.com>
2023-05-12 16:57:35 +0900

    refactor: 카메라 초기값 전방 -> 후방으로 변경

--- presentation/src/main/java/team/jsv/icec/ui/camera/CameraViewModel.kt
+++ presentation/src/main/java/team/jsv/icec/ui/camera/CameraViewModel.kt
@@ -13,7 +13,7 @@ class CameraViewModel : BaseViewModel() {
     private val _bitmapImage = MutableLiveData<Bitmap>()
     val bitmapImage: LiveData<Bitmap> get() = _bitmapImage
 
-    private val _isFrontCamera = MutableLiveData<Boolean>().apply { value = true }
+    private val _isFrontCamera = MutableLiveData<Boolean>().apply { value = false }
     val isFrontCamera: LiveData<Boolean> get() = _isFrontCamera
 
     fun setRatioState(value: Int) {
