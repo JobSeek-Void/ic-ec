c9a82ea168af1b1c57fad232013fe3f2ce88410d
DESKTOP-QIC63AA\user <wldbs5897@naver.com>
2023-05-14 14:34:10 +0900

    feat: 갤러리 연동하기

--- presentation/src/main/java/team/jsv/icec/ui/main/StartActivity.kt
+++ presentation/src/main/java/team/jsv/icec/ui/main/StartActivity.kt
@@ -1,7 +1,12 @@
 package team.jsv.icec.ui.main
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import team.jsv.icec.base.BaseActivity
 import team.jsv.icec.ui.camera.CameraActivity
 import team.jsv.icec.util.PermissionUtil
@@ -10,9 +15,22 @@ import team.jsv.presentation.R
 import team.jsv.presentation.databinding.ActivityStartBinding
 
 class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start) {
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestPermissions(PermissionUtil.getPermissions())
+
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+
+                if (data != null) {
+                    val imageUri: Uri? = data.data
+                    //Todo(jiiiiiyoon): imageUri 모자이크 화면으로 넘기기
+                }
+            }
+        }
     }
 
     override fun onResume() {
@@ -22,11 +40,16 @@ class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start
 
     private fun initClickEvent() {
         binding.btPhoto.setOnClickListener {
-            //Todo(jiiiiyoon): 갤러리 연동하기
+            openGallery()
         }
 
         binding.btCamera.setOnClickListener {
             startActivity(Intent(this@StartActivity, CameraActivity::class.java))
         }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        imagePickerLauncher.launch(galleryIntent)
     }
 }
