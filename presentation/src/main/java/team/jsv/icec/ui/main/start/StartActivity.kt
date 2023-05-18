6248ea82c6e597d446dafd2dfd176696e12c0932
jiiiiiyoon <wldbs5897@naver.com>
2023-05-18 22:14:01 +0900

    refactor: 패키지 처리

--- presentation/src/main/java/team/jsv/icec/ui/main/start/StartActivity.kt
+++ presentation/src/main/java/team/jsv/icec/ui/main/start/StartActivity.kt
@@ -1,14 +1,22 @@
-package team.jsv.icec.ui.main
+package team.jsv.icec.ui.main.start
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import team.jsv.icec.base.BaseActivity
 import team.jsv.icec.ui.camera.CameraActivity
+import team.jsv.icec.ui.main.start.adapter.GalleryImageAdapter
+import team.jsv.icec.ui.main.start.model.GalleryImageItem
 import team.jsv.icec.util.PermissionUtil
 import team.jsv.icec.util.requestPermissions
 import team.jsv.presentation.R
@@ -16,21 +24,27 @@ import team.jsv.presentation.databinding.ActivityStartBinding
 
 class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start) {
     private lateinit var imagePickerLauncher: ActivityResultLauncher<Intent>
+    private lateinit var galleryImageAdapter: GalleryImageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestPermissions(PermissionUtil.getPermissions())
 
-        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
+        imagePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
 
-                if (data != null) {
-                    val imageUri: Uri? = data.data
-                    //Todo(jiiiiiyoon): imageUri 모자이크 화면으로 넘기기
+                    if (data != null) {
+                        val imageUri: Uri? = data.data
+                        //Todo(jiiiiiyoon): imageUri 모자이크 화면으로 넘기기
+                    }
                 }
             }
+
+
         }
+
     }
 
     override fun onResume() {
