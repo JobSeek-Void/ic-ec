dd859c713d69781c5ad53a26f10fdd90cb915d7a
jiiiiiyoon <wldbs5897@naver.com>
2023-06-07 18:30:44 +0900

    refactor: API 버전에 따른 상태바 숨김 설정

--- presentation/src/main/java/team/jsv/icec/ui/camera/CameraActivity.kt
+++ presentation/src/main/java/team/jsv/icec/ui/camera/CameraActivity.kt
@@ -18,6 +18,37 @@ class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_cam
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestPermissions(PermissionUtil.getPermissions())
+    }
+
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+            setBottomMargin()
+        } else {
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+        }
+    }
+
+    private fun setBottomMargin() {
+        val layoutParams =
+            binding.cameraNavHostFragment.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.setMargins(
+            0, 0, 0,
+            SettingViewUtil.getNavigationBarHeightDP(this)
+        )
+        binding.cameraNavHostFragment.layoutParams = layoutParams
+    }
+
     companion object {
         const val RESOURCE_STATUS_NAME = "status_bar_height"
         const val RESOURCE_NAVIGATION_NAME = "navigation_bar_height"
