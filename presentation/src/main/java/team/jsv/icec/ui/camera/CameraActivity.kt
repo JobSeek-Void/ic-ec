6a280319acdfa5dae324598a179c8aa84a1c7d8e
jiiiiiyoon <wldbs5897@naver.com>
2023-06-08 15:34:02 +0900

    refactor: 리뷰 반영 @EvergreenTree97 - hideSystemUI Util 확장함수로 변경

--- presentation/src/main/java/team/jsv/icec/ui/camera/CameraActivity.kt
+++ presentation/src/main/java/team/jsv/icec/ui/camera/CameraActivity.kt
@@ -1,14 +1,9 @@
 package team.jsv.icec.ui.camera
 
-import android.os.Build
-import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowInsets
 import team.jsv.icec.base.BaseActivity
 import team.jsv.icec.util.PermissionUtil
-import team.jsv.icec.util.SettingViewUtil
+import team.jsv.icec.util.hideSystemUI
 import team.jsv.icec.util.requestPermissions
 import team.jsv.presentation.R
 import team.jsv.presentation.databinding.ActivityCameraBinding
@@ -20,33 +15,9 @@ class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_cam
         requestPermissions(PermissionUtil.getPermissions())
     }
 
-
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        if (Build.VERSION.SDK_INT >= VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-            setBottomMargin()
-        } else {
-            window.decorView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
-        }
-    }
-
-    private fun setBottomMargin() {
-        val layoutParams =
-            binding.cameraNavHostFragment.layoutParams as ViewGroup.MarginLayoutParams
-        layoutParams.setMargins(
-            0, 0, 0,
-            SettingViewUtil.getNavigationBarHeightDP(this)
-        )
-        binding.cameraNavHostFragment.layoutParams = layoutParams
+        hideSystemUI(binding.cameraNavHostFragment)
     }
 
     companion object {
