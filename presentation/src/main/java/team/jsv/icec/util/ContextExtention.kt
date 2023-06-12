2727508ec7ba6281bd8c7d5be5b4cbacecc2a7ea
GunHyung Ham <gdreagon@naver.com>
2023-06-12 16:30:44 +0900

    feat : `Context` 확장함수 `getPathFromUri()` 제작

--- presentation/src/main/java/team/jsv/icec/util/ContextExtention.kt
+++ presentation/src/main/java/team/jsv/icec/util/ContextExtention.kt
@@ -2,7 +2,9 @@ package team.jsv.icec.util
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import android.provider.MediaStore
 import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import team.jsv.icec.ui.main.mosaic.result.MosaicResultActivity
@@ -38,4 +40,19 @@ fun Context.shareImage(
     }
 
     this.startActivity(Intent.createChooser(shareIntent, this.getString(R.string.share_text)))
+}
+
+/**
+ * [Context] 확장 함수 이미지의 절대 경로를 가져오는 함수 입니다.
+ * @param uri 이미지의 content URI
+ * EX) "content://media/external/images/media/1000000150"
+ * @return [String] 이미지의 절대 경로를 반환합니다. null을 반환할 수 있으니 주의하세요.
+ */
+fun Context.getPathFromUri(uri: Uri): String? {
+    val projection = arrayOf(MediaStore.Images.Media.DATA)
+    val cursor = this.contentResolver.query(uri, projection, null, null, null)
+    return cursor?.use {
+        val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        if (cursor.moveToFirst()) cursor.getString(columnIndex) else null
+    }
 }
