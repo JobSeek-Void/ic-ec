82cef39cd65ee445897bb84e4505bdbd6a6ea46a
GunHyung Ham <gdreagon@naver.com>
2023-06-22 15:02:33 +0900

    feat : 액티비티 확장함수 `hideSystemUI()` 제작

--- presentation/src/main/java/team/jsv/icec/util/activityUtils.kt
+++ presentation/src/main/java/team/jsv/icec/util/activityUtils.kt
@@ -106,4 +106,19 @@ fun Activity.showSystemUI() {
     }
 }
 
+fun Activity.hideSystemUI() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.insetsController?.let {
+            it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    } else {
+        @Suppress("DEPRECATION")
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
 }
