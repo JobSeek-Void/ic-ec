3e626dc2f6e967c87f8b1fbbb9d1d34dee560fc5
GunHyung Ham <gdreagon@naver.com>
2023-06-23 15:14:00 +0900

    refactor : `CustomTypefaceSpan` 클래스 리팩토링 -  @EvergreenTree97 리뷰 반영

--- presentation/src/main/java/team/jsv/icec/util/CustomTypefaceSpan.kt
+++ presentation/src/main/java/team/jsv/icec/util/CustomTypefaceSpan.kt
@@ -6,25 +6,25 @@ import android.text.TextPaint
 import android.text.style.TypefaceSpan
 
 class CustomTypefaceSpan(family: String, private val newType: Typeface) : TypefaceSpan(family) {
-    override fun updateDrawState(ds: TextPaint) {
-        applyCustomTypeFace(ds, newType)
+    override fun updateDrawState(paint: TextPaint) {
+        applyCustomTypeFace(paint, newType)
     }
 
     override fun updateMeasureState(paint: TextPaint) {
         applyCustomTypeFace(paint, newType)
     }
 
-    private fun applyCustomTypeFace(paint: Paint, tf: Typeface) {
+    private fun applyCustomTypeFace(paint: Paint, typeface: Typeface) {
         val oldStyle: Int
         val old = paint.typeface
         oldStyle = old?.style ?: 0
-        val fake = oldStyle and tf.style.inv()
+        val fake = oldStyle and typeface.style.inv()
         if (fake and Typeface.BOLD != 0) {
             paint.isFakeBoldText = true
         }
         if (fake and Typeface.ITALIC != 0) {
             paint.textSkewX = -0.25f
         }
-        paint.typeface = tf
+        paint.typeface = typeface
     }
 }
