630518c8244229a940f9b0724b83cb7528463818
jiiiiiyoon <wldbs5897@naver.com>
2023-07-06 17:21:35 +0900

    feat: 최근 항목 recyclerView UI작업(하드코딩)

--- presentation/src/main/java/team/jsv/icec/ui/main/start/StartActivity.kt
+++ presentation/src/main/java/team/jsv/icec/ui/main/start/StartActivity.kt
@@ -2,11 +2,13 @@ package team.jsv.icec.ui.main.start
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Resources
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.GridLayoutManager
 import team.jsv.icec.base.BaseActivity
 import team.jsv.icec.base.startActivityWithAnimation
 import team.jsv.icec.ui.camera.CameraActivity
@@ -27,7 +29,41 @@ class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start
 
         requestPermissions(PermissionUtil.getPermissions())
         setImagePickerLauncher()
+
+        val projection =
+            arrayOf(MediaStore.Images.Media.DATA, MediaStore.Images.Media.DATE_MODIFIED)
+        val cursor = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            "${MediaStore.Images.Media.DATE_MODIFIED} DESC"
+        )
+
+        val imagePaths = mutableListOf<String>()
+        cursor?.use {
+            val dataIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            var count = 0
+
+            while (it.moveToNext() && count < 30) {
+                val imagePath = it.getString(dataIndex)
+                imagePaths.add(imagePath)
+                count++
+            }
+        }
+
+        cursor?.close()
+
+        binding.rvRecentImage.layoutManager = GridLayoutManager(this, 3)
+        val adapter = ImageAdapter(this, imagePaths)
+        binding.rvRecentImage.adapter = adapter
+        binding.rvRecentImage.addItemDecoration(
+            GridSpacingItemDecoration(spanCount = 3, spacing = 16f.fromDpToPx())
+        )
     }
+
+    private fun Float.fromDpToPx(): Int =
+        (this * Resources.getSystem().displayMetrics.density).toInt()
 
     override fun onResume() {
         super.onResume()
